0d68a76644c7d3c279ca975918a84d77b955203b
Axle <95949634+axlecoffee@users.noreply.github.com>
2025-12-28 16:07:44 -0500

    evil

--- src/main/java/coffee/axle/myauinjectiontemplate/util/TestModuleRenderer.java
+++ src/main/java/coffee/axle/myauinjectiontemplate/util/TestModuleRenderer.java
@@ -11,9 +11,9 @@ import net.minecraft.util.Vec3;
 import org.lwjgl.opengl.GL11;
 
 /**
- * Static renderer for TestModule - handles both mixin and direct rendering
- * paths
+ * Test module - originally seen in CoffeeClient to test if a mixin worked - proof of concept "hey injection works" ahh
  */
+
 public class TestModuleRenderer {
     private static final Minecraft mc = Minecraft.getMinecraft();
 
@@ -37,7 +37,7 @@ public class TestModuleRenderer {
     }
 
     /**
-     * Called from mixin - renders GREEN box when mixin path is active
+     * Called from mixin - renders GREEN box when mixin-enabled = true
      */
     public static void renderMixinBox(float partialTicks) {
         if (!moduleEnabled || !mixinEnabled) {
@@ -55,7 +55,7 @@ public class TestModuleRenderer {
     }
 
     /**
-     * Called from Forge event - renders PURPLE box when direct path is active
+     * Called from Forge event - renders PURPLE box when mixin-enabled = false
      */
     public static void renderDirectBox(float partialTicks) {
         if (!moduleEnabled || mixinEnabled) {
@@ -67,34 +67,27 @@ public class TestModuleRenderer {
 
         AxisAlignedBB renderBox = calculateBoxPosition(partialTicks);
 
-        // Purple box for direct rendering
         drawFilledBox(renderBox, 0.5f, 0.0f, 0.8f, 0.4f);
         drawOutlinedBox(renderBox, 0.8f, 0.2f, 1.0f, 1.0f);
     }
 
     private static AxisAlignedBB calculateBoxPosition(float partialTicks) {
         Entity player = mc.thePlayer;
-
-        // Get player look direction
         Vec3 lookVec = player.getLookVec();
 
-        // Calculate interpolated player position
         double playerX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double playerY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double playerZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
 
-        // Position 3 blocks in front
         double boxX = playerX + lookVec.xCoord * 3.0;
         double boxY = playerY + lookVec.yCoord * 3.0 + player.getEyeHeight();
         double boxZ = playerZ + lookVec.zCoord * 3.0;
 
-        // Create bounding box (1x1x1 cube centered at position)
         double size = 0.5;
         AxisAlignedBB box = new AxisAlignedBB(
                 boxX - size, boxY - size, boxZ - size,
                 boxX + size, boxY + size, boxZ + size);
 
-        // Offset by render position
         double renderX = mc.getRenderManager().viewerPosX;
         double renderY = mc.getRenderManager().viewerPosY;
         double renderZ = mc.getRenderManager().viewerPosZ;
@@ -115,37 +108,37 @@ public class TestModuleRenderer {
 
         worldRenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
 
-        // Bottom face
+        // B
         worldRenderer.pos(box.minX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
 
-        // Top face
+        // T
         worldRenderer.pos(box.minX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
 
-        // North face
+        // N
         worldRenderer.pos(box.minX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
 
-        // South face
+        // S
         worldRenderer.pos(box.minX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
 
-        // West face
+        // W
         worldRenderer.pos(box.minX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
 
-        // East face
+        // E
         worldRenderer.pos(box.maxX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
@@ -173,7 +166,7 @@ public class TestModuleRenderer {
 
         worldRenderer.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION_COLOR);
 
-        // Bottom face
+        // Bottom
         worldRenderer.pos(box.minX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.minY, box.maxZ).color(red, green, blue, alpha).endVertex();
@@ -184,7 +177,7 @@ public class TestModuleRenderer {
 
         worldRenderer.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION_COLOR);
 
-        // Top face
+        // Top
         worldRenderer.pos(box.minX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.maxX, box.maxY, box.maxZ).color(red, green, blue, alpha).endVertex();
@@ -195,7 +188,7 @@ public class TestModuleRenderer {
 
         worldRenderer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
 
-        // Vertical edges
+        // vert edging
         worldRenderer.pos(box.minX, box.minY, box.minZ).color(red, green, blue, alpha).endVertex();
         worldRenderer.pos(box.minX, box.maxY, box.minZ).color(red, green, blue, alpha).endVertex();
 
